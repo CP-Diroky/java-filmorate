740e0bcad93abb060ae6bb6f282b34333f384e38
Alexandr <Alexapugachev@yandex.ru>
2026-04-16 14:52:32 +0300

    Добавлена функциональность "Общие фильмы"

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -3,6 +3,7 @@ package ru.yandex.practicum.filmorate.storage;
 import ru.yandex.practicum.filmorate.model.Film;
 
 import java.util.Collection;
+import java.util.List;
 
 public interface FilmStorage {
 
@@ -20,4 +21,5 @@ public interface FilmStorage {
 
     Collection<Film> getPopularFilms(int count);
 
+    List<Film> getCommonFilms(Long userId, Long friendId);
 }
